40e81be8364d25de34604e2b06953f40c9f8fc69
RCowles <contact@ryanscowles.com>
2013-03-28 13:12:55 -0700

    Update to 1.0.1
    This update will use the fullsize image, instead of the thumbnail size.

--- README.txt
+++ README.txt
@@ -3,16 +3,16 @@ Contributors: metacomcreative
 Donate link: http://ryanscowles.com/
 Tags: open graph, meta tags, facebook, featured image
 Requires at least: 3.5
-Tested up to: 3.5
-Stable tag: 1.0
+Tested up to: 3.5.1
+Stable tag: 1.0.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-This plugin will place Open Graph meta tags in your head with automatically generated values. 
+This plugin will place automatically generated Open Graph meta tags in your head.
 
 == Description ==
 
-This plugin will add the necessary Open Graph Meta tags to automatically set the posts' Featured Image as the thumbnail for sharing on Facebook and LinkedIn. It will also set Open Graph Meta tags for title, description, URL, type, and site name. 
+This plugin will add the necessary Open Graph Meta tags to automatically set the posts' Featured Image as the thumbnail for sharing on Facebook and LinkedIn. It will also set Open Graph Meta tags for title, description, URL, type, and site name.
 
 
 == Installation ==
@@ -39,7 +39,13 @@ The plugin use WordPress Template tags to fill in the values for the Meta tags a
 
 == Changelog ==
 
+= 1.0.1 =
+* Use full size image instead of thumbnail for og:image tag.
+
 = 1.0 =
 * Initial commit
 
 == Upgrade Notice ==
+
+= 1.0.1 =
+Use full size image instead of thumbnail for og:image tag.
--- fb-featured-image.php
+++ fb-featured-image.php
@@ -1,11 +1,27 @@
 <?php
 /*
 Plugin Name: Facebook Featured Image and Open Graph Meta Tags
-Version: 1.0
-Plugin URI: http://www.ryanscowles.com
-Description: This plugin will add the necessary Open Graph Meta tags to automatically set the posts' Featured Image as the thumbnail for sharing on Facebook and LinkedIn. It will also set Open Graph Meta tags for title, description, URL, type, and site name. 
+Version: 1.0.1
+Plugin URI: http://ryanscowles.com/2013/01/set-wordpress-featured-image-thumbnail/
+Description: This plugin will add the necessary Open Graph Meta tags to automatically set the posts' Featured Image as the thumbnail for sharing on Facebook and LinkedIn. It will also set Open Graph Meta tags for title, description, URL, type, and site name.
 Author: Ryan S. Cowles
 Author URI: http://www.ryanscowles.com
+License: GPL2
+
+	Copyright 2013  Ryan Cowles  (email : mr@ryanscowles.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 define ('pluginDirName', 'fb-featured-image');
@@ -29,7 +45,7 @@ function fbogmeta_header() {
 		<meta property="og:title" content="<?php the_title(); ?>"/>
 		<meta property="og:description" content="<?php echo strip_tags(get_the_excerpt($post->ID)); ?>" />
 		<meta property="og:url" content="<?php the_permalink(); ?>"/>
-		<?php $fb_image = wp_get_attachment_image_src(get_post_thumbnail_id( get_the_ID() ), 'thumbnail'); ?>
+		<?php $fb_image = wp_get_attachment_image_src(get_post_thumbnail_id( get_the_ID() ), 'full'); ?>
 		<?php if ($fb_image) : ?>
 			<meta property="og:image" content="<?php echo $fb_image[0]; ?>" />
 			<?php endif; ?>
@@ -38,7 +54,7 @@ function fbogmeta_header() {
 		/>
 		<meta property="og:site_name" content="<?php bloginfo('name'); ?>"/>
 		<!-- End Open Graph Meta Tags !-->
-		
+
     <?php
   }
 }
